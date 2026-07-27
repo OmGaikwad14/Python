71db33aab6d00ac5abebf0fc4df52897d13e79cf
Om Gaikwad <omgaikwad140@gmail.com>
2026-07-27 23:28:02 +0530

    Uncomment Python practice questions in practiceQue.py

--- 02_Strings & Conditional Statements/practiceQue.py
+++ 02_Strings & Conditional Statements/practiceQue.py
@@ -1,46 +1,46 @@
 # Strings
 # 1. WAP to input user’s first name & print its length.
 
-# name = str(input("Enter your name: "))
+name = str(input("Enter your name: "))
 
-# print("Length of your name is", len(name))
+print("Length of your name is", len(name))
 
 # 2. WAP to find the occurrence of ‘$’ in a String.
 
-# str = "Hi, $Iam the $ symbol $99.99"
+str = "Hi, $Iam the $ symbol $99.99"
 
-# print(str.count('$'))
+print(str.count('$'))
 
 # Conditional Statement
 # 1. WAP to check if a number entered by the user is odd or even.
 
-# num = int(input("Enter number: "))
+num = int(input("Enter number: "))
 
-# rem = num % 2
+rem = num % 2
 
-# if(rem == 0):
-#     print("EVEN")
-# else:
-#     print("ODD")
+if(rem == 0):
+    print("EVEN")
+else:
+    print("ODD")
 
 # # 2. WAP to find the greatest of 3 numbers entered by the user.
 
-# a = int(input("Enter first number: "))
-# b = int(input("Enter second number: "))
-# c = int(input("Enter third number: "))
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+c = int(input("Enter third number: "))
 
-# if(a >= b and b >= c):
-#     print("first number is largest", a)
-# elif(b >= c):
-#     print("second number is largest", b)
-# else:
-#     print("third number is largest", c)
+if(a >= b and b >= c):
+    print("first number is largest", a)
+elif(b >= c):
+    print("second number is largest", b)
+else:
+    print("third number is largest", c)
 
 # # 3. WAP to check if a number is a multiple of 7 or not.
 
-# x = int(input("Enter number: "))
+x = int(input("Enter number: "))
 
-# if(x % 7 == 0):
-#     print("Multiple of 7")
-# else:
-#     print("Not a multiple")
+if(x % 7 == 0):
+    print("Multiple of 7")
+else:
+    print("Not a multiple")
