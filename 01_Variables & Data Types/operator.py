51bcc873c0dd9f49d7fa3a88239bb2cc4323892e
Om Gaikwad <omgaikwad140@gmail.com>
2026-07-30 11:49:10 +0530

    Uncomment arithmetic, relational, and logical operations

--- 01_Variables & Data Types/operator.py
+++ 01_Variables & Data Types/operator.py
@@ -3,49 +3,49 @@
 a = 5
 b = 2
 
-# print(a + b)
-# print(a - b)
-# print(a * b)
-# print(a / b)
-# print(a ** b) # a^b power
-# print(a % b) # remainder
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a ** b) # a^b power
+print(a % b) # remainder
 
 # Relational/Comparison Operator
 
 a = 50
 b = 20
 
-# print(a == b)
-# print(a != b) # ! - NOT
-# print(a <= b)
-# print(a >= b)
-# print(a < b)
-# print(a > b)
+print(a == b)
+print(a != b) # ! - NOT
+print(a <= b)
+print(a >= b)
+print(a < b)
+print(a > b)
 
 # Assignment Operator
 
 num = 10
 
-# num += 10
-# num -= 10
-# num *= 10
-# num /= 10
-# num **= 10
-# num %= 10
+num += 10
+num -= 10
+num *= 10
+num /= 10
+num **= 10
+num %= 10
 
-# print("num :", num)
+print("num :", num)
 
 # Logical Operator
 
 a = 50
 b = 30
 
-# print(not True)
-# print(not False)
-# print(not (a > b))
+print(not True)
+print(not False)
+print(not (a > b))
 
 val1 = True
 val2 = False
 
-# print("AND Operator :", val1 and val2)
-# print("OR Operator :", val1 or val2)
+print("AND Operator :", val1 and val2)
+print("OR Operator :", val1 or val2)
