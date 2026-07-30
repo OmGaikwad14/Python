376b9694834d7bea2fde7564cb81c21a18de77d6
Om Gaikwad <omgaikwad140@gmail.com>
2026-07-30 11:47:41 +0530

    Uncomment code for type conversion examples

--- 01_Variables & Data Types/conversion.py
+++ 01_Variables & Data Types/conversion.py
@@ -3,18 +3,18 @@
 a = 2
 b = 4.25
 
-# sum = a + b
-# print(sum)
+sum = a + b
+print(sum)
 
 # Casting conversion - mannual convert
 
 x = int("2")
 y = 4.25
 
-# print(type(x))
-# print(x + y)
+print(type(x))
+print(x + y)
 
 a = 3.14
 a = str(a)
 
-# print(type(a))
+print(type(a))
