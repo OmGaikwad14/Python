e80078983f7529483d19f56d19b9e498ec03b983
Om Gaikwad <omgaikwad140@gmail.com>
2026-07-30 11:49:31 +0530

    Uncommented print statements and added age2 output

--- 01_Variables & Data Types/variables.py
+++ 01_Variables & Data Types/variables.py
@@ -4,10 +4,10 @@ city = "Mumbai"
 price = 25.99
 
 age2 = age
-# print(age2)
+print(age2)
 
-# print("My name is :", name)
-# print("My age is :", age)
+print("My name is :", name)
+print("My age is :", age)
 
 print(type(name))
 print(type(age))
